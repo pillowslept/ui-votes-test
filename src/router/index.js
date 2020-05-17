55b93a1abde627c5df72313350c098036caf0d99
Juan Camilo Velásquez Vanegas <juancamilo---142@hotmail.com>
2020-05-16 23:01:09 -0500

    Login and PastTrials router views.

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,16 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/past-trials',
+    name: 'PastTrials',
+    component: () => import(/* webpackChunkName: "about" */ '../views/PastTrials.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   }
 ]
 
---
+++ src/views/PastTrials.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="login">
+    <h1>This is the past trials page</h1>
+  </div>
+</template>
